56840603efe022801a34e249e4b4b567601fda03
pretyfx68 <fadlan040909@gmail.com>
2026-04-12 06:43:32 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -7346,7 +7346,7 @@ function ztSetFmt(btn, fmt) {
 }
 
 // Auto-fetch URL tunnel dari server
-const HARDCODED_BACKEND_URL = 'https://biodiversity-switching-bruce-meals.trycloudflare.com';
+const HARDCODED_BACKEND_URL = 'https://senior-helena-focal-baby.trycloudflare.com';
 let PROXY_URL = localStorage.getItem('vidsnap_proxy_url') || HARDCODED_BACKEND_URL;
 
 // Otomatis update PROXY_URL dari server setiap buka halaman
