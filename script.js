13550dde139aef3f5507279225f0197a036c6188
pretyfx68 <fadlan040909@gmail.com>
2026-06-24 12:47:25 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -8524,7 +8524,7 @@ function ztSetFmt(btn, fmt) {
 }
 
 // Auto-fetch URL tunnel dari server
-const HARDCODED_BACKEND_URL = 'https://highs-using-anyone-thing.trycloudflare.com';
+const HARDCODED_BACKEND_URL = 'https://utc-optional-monthly-ins.trycloudflare.com';
 let PROXY_URL = localStorage.getItem('vidsnap_proxy_url') || HARDCODED_BACKEND_URL;
 
 // Otomatis update PROXY_URL dari server setiap buka halaman
