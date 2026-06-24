3964a200ce4ec769e860bbc98fe16b6b8f0ae0b2
pretyfx68 <fadlan040909@gmail.com>
2026-06-24 21:21:53 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -8524,7 +8524,7 @@ function ztSetFmt(btn, fmt) {
 }
 
 // Auto-fetch URL tunnel dari server
-const HARDCODED_BACKEND_URL = 'https://gem-mumbai-overnight-ended.trycloudflare.com';
+const HARDCODED_BACKEND_URL = 'https://gas-frog-eng-posters.trycloudflare.com';
 let PROXY_URL = localStorage.getItem('vidsnap_proxy_url') || HARDCODED_BACKEND_URL;
 
 // Otomatis update PROXY_URL dari server setiap buka halaman
